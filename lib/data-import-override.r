c8809adb27a4f76780285c10ebda347997cf546d
pbdanny <pb.danny@gmail.com>
2017-11-09 03:34:41 +0700

    Cache reason desc

--- lib/data-import-override.r
+++ lib/data-import-override.r
@@ -1,7 +1,15 @@
-# Add alternative data importation commands for files in 
-# the data folder that should not be imported using standard data importation rules
+# Data manipulations
+# Highlight and run command below do load project without munging.
+# This is useful when debugging data manipulation code.
+# rm(list = ls()); library(ProjectTemplate); load.project(list(munging=FALSE))
+
 df <- read_tsv(file.path(getwd(), 'raw-data', 'OSS_RL_Aug17_Sep17.tsv.zip'), 
                guess_max = 5000)
 colnames(df) <- make.names(tolower(colnames(df)), 
                            unique = TRUE,
                            allow_ = TRUE)
+
+reason_desc <- read_tsv(file.path(getwd(), 'raw-data', 'reason_desc.txt'))
+colnames(reason_desc) <- make.names(tolower(colnames(reason_desc)), 
+                                    unique = TRUE,
+                                    allow_ = TRUE)
--- munge/01-munge.R
+++ munge/01-munge.R
@@ -1,12 +1,3 @@
-# Add alternative data importation commands for files in 
-# the data folder that should not be imported using standard data importation rules
-df <- read_tsv(file.path(getwd(), 'raw-data', 'OSS_RL_Aug17_Sep17.tsv.zip'), 
-               guess_max = 5000)
-colnames(df) <- make.names(tolower(colnames(df)), 
-                           unique = TRUE,
-                           allow_ = TRUE)
-
-reason_desc <- read_tsv(file.path(getwd(), 'raw-data', 'reason_desc.txt'))
-colnames(reason_desc) <- make.names(tolower(colnames(reason_desc)), 
-                                    unique = TRUE,
-                                    allow_ = TRUE)
+rl_os <- df %>%
+    filter(channel == 'OSS')
+cache('df'); rm(df)
